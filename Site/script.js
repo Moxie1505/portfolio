ea1ab370ece7709f646c91c2a4bcc0e1e3205482
milianid <djibril.miliani@etu.univ-grenoble-alpes.fr>
2024-09-24 20:00:42 +0200

    hey2

--- Site/script.js
+++ Site/script.js
@@ -1,37 +1,34 @@
-
 function enterPortfolio() {
-    const landingPage = document.getElementById('landing-page');
-    const portfolioContent = document.getElementById('portfolio-content');
-    
-    landingPage.style.opacity = 0;
-    landingPage.style.transform = 'scale(0.9)';
-    
+  const landingPage = document.getElementById('landing-page');
+  const portfolioContent = document.getElementById('portfolio-content');
+
+  landingPage.classList.add('reduced');
+
+  setTimeout(() => {
+    landingPage.style.display = 'none';  
+    portfolioContent.style.display = 'block';  
+
     setTimeout(() => {
-        landingPage.style.display = 'none';
-        portfolioContent.style.display = 'block';
-        
-        setTimeout(() => {
-            portfolioContent.classList.add('show');
-        }, 100); 
-    }, 1000);
+      portfolioContent.classList.add('show');
+    }, 100);
+  }, 1000);  
 }
 
 function openPopup(id) {
-    var popup = document.getElementById(id);
-    popup.style.display = 'block';
-    document.body.classList.add('no-scroll');
-  }
+  const popup = document.getElementById(id);
+  popup.style.display = 'block';  
+  document.body.classList.add('no-scroll');  
+}
 
-  function closePopup(id) {
-    var popup = document.getElementById(id);
-    popup.style.display = 'none';
-    document.body.classList.remove('no-scroll');
-  }
+function closePopup(id) {
+  const popup = document.getElementById(id);
+  popup.style.display = 'none';  
+  document.body.classList.remove('no-scroll');  
+}
 
-  window.onclick = function(event) {
-    if (event.target.classList.contains('popup')) {
-      event.target.style.display = 'none';
-      document.body.classList.remove('no-scroll');
-    }
+window.onclick = function(event) {
+  if (event.target.classList.contains('popup')) {
+    event.target.style.display = 'none';  
+    document.body.classList.remove('no-scroll');  
   }
-
+};
